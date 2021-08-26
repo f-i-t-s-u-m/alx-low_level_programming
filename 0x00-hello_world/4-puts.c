31d2eb91efdfdac4cf83ee5d7f3698085e3ed1c2
f-i-t-s-u-m <fibabirr@gmail.com>
2021-08-26 11:46:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
+ * Return : just Success
  */
 int main(void)
 {
 
-char fiba[] = "Programming is like building a multilingual puzzle";
+char fiba[] = "\"Programming is like building a multilingual puzzle";
 puts(fiba);
 return (0);
 
