417d959d883fadec44b4593579d06d047352e6c1
f-i-t-s-u-m <fibabirr@gmail.com>
2021-08-31 07:59:01 +0300

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,14 +7,16 @@
 
 int main(void)
 {
-	char c[9] = "Holberton";
+        _putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-	int i = 0;
-
-	while (i < 9)
-	{
-		_putchar(c[i]);
-		++i;
-	}
 	return (0);
 }
