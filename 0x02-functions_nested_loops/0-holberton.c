4efbea3b343788f1797bfd30f168d688222502ab
f-i-t-s-u-m <fibabirr@gmail.com>
2021-08-31 05:04:49 +0300

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,16 +1,21 @@
 #include "main.h"
+/**
+ * main - Entry block
+ * Description: prints holberton on new line
+ * Return: 0
+ */
 
 int main(void)
 {
-   _putchar('H');
-   _putchar('o');
-   _putchar('l');
-   _putchar('b');
-   _putchar('e');
-   _putchar('r');
-   _putchar('t');
-   _putchar('o');
-   _putchar('n');
-   _putchar('\n');
-  return (0);
+	char c[10] = "Holberton";
+
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
