947fa4a18b199ad43ae4be35014353dc764cf3d9
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-08 15:40:52 +0300

    update 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * char *_strcat(char *dest, char *src) - start here
+ * Return:pinter
+ * 
+*/
+
+char *_strcat(char *dest, char *src)
+{
+  
+  strcat (dest, src);
+  return (dest);
+}
