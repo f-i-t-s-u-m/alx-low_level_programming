0700d0e79950ad7744c21f362d66ce87731860b6
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-13 22:34:23 +0300

    update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,11 +8,12 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int n = 0, t = 0;
-while (accept[n] != '\0')
+while (accept[n])
 {
 int a;
 
 for (a = 0; s[a] != 32; a++)
+{
 if (accept[n] == s[a])
 {
 t++;
