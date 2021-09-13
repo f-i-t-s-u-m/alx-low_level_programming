8ae6107fc8923ef43e63e3aff54c050b70e25f15
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-13 20:56:57 +0300

    update 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strchr - function start here
+ *Return: array
+ * @s: array
+ * @c: char
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+char *null = NULL;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (*(s + i) == c)
+{
+return (s + i);
+}
+}
+if (*(s + i) == 0)
+{
+return (s + i);
+}
+else
+return (null);
+}
