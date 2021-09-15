1dd8e5fd8627bf83ce9a26820ac05749bcef85f7
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-15 15:29:30 +0300

    update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
-
-
+/**
+ *_puts_recursion - function starting point
+ *@s: pointer
+ *
+ */
 void _puts_recursion(char *s)
 {
-
-  if(*(s) == '\0')
-    {
-      _putchar('\n');
-      return;
-    }
-  _putchar(*(s));
-  _puts_recursion(& *(s+1));
-
+if (*(s) == '\0')
+{
+_putchar ('\n');
+return;
+}
+_putchar (*(s));
+_puts_recursion(&*(s + 1));
 }
