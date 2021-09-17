dc2e8708a8f8e7cc7ee71e9633731780d11f1f6b
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-17 16:02:32 +0300

    update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  *main - function starting point
@@ -10,3 +11,4 @@ int main(int argc __attribute__((unused)), char *argv[])
 	printf("%s\n", argv[0]);
 	return (1);
 }
+
