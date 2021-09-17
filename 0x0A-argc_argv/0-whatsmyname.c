8320391c460f07609f98e82d6d43241a1c0ba3de
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-17 11:13:50 +0300

    update 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - function starting point
+ *@argc: arg length
+ *@argv: array of agr
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
