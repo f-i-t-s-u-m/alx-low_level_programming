c58d7f7544842eac304dc78c8770ce656cb7aa7d
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-17 11:22:13 +0300

    create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - function starting point
+ *@argc: arg length
+ *@argv: array of agr
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+}
