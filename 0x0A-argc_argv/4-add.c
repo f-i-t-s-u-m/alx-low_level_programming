6d93b33028cbb622adf72bd0749914e190850469
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-17 16:22:04 +0300

    update 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function starting point
+ *@argc: arg length
+ *@argv: array of agr
+ *Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i, m = 0;
+
+	if (argc < 1)
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+
+	if (atoi(argv[i]) == 0)
+	{
+	printf("%s\n", "Error");
+	return (1);
+	}
+	m = m + atoi(argv[i]);
+	}
+	printf("%d\n", m);
+	return (0);
+}
