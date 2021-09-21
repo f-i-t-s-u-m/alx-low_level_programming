deff3ee369f846cc024936b304fbc79afdd4ff35
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-21 11:21:40 +0300

    update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,7 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *ptr = (char *)malloc(size);
+	char *ptr = (char *)malloc(size * sizeof(c));
 
 	for (i = 0; i < size; i++)
 	ptr[i] = c;
