edd7cddf0f28359c7f1595652e6f4146b049b590
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-22 00:48:36 +0300

    update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, k = 0, n, l;
+	int i, k = 0, l = 0, n;
 	char *ptr;
 
 	if (ac == 0 || av == NULL)
@@ -21,12 +21,17 @@ char *argstostr(int ac, char **av)
 		for (j = 0; av[i][j] != '\0'; j++, k++)
 		;
 	}
-	ptr = (char *)malloc((k * i) + 4);
-	for(n = 0; n < ac + 4; n++, l++)
+	
+	ptr = (char *)malloc(k + ac);
+	for(n = 0; n < ac; n++)
 	{
 		int z;
-		for(z = 0; av[n][z] != '\0'; z++ )
-		ptr[l] = av[n][z];
+		for(z = 0; av[n][z] != '\0'; z++, l++ )
+		{
+			ptr[l] = av[n][z];
+		}
+		ptr[l] = '\n';
+		l++;
 	}
 	return (ptr);
 }
