fbd540a7907a2c41de4b8541a681d59ad4de8262
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-21 13:07:32 +0300

    update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,21 +1,43 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * _strdup - function start here
- *@str: string
- *Return: char
+ * _stlen - function start here
+ *@s: string
+ *Return: number of char
  */
+int _stlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+		a++;
+	return (a);
+}
+
+/**
+* _strdup - function start here
+*@str: string
+*Return: char
+*/
+
 char *_strdup(char *str)
 {
 
 	unsigned int i;
-	char *ptr = (char *)malloc(sizeof(str));
+	char *ptr = (char *)malloc(_stlen(str));
 
 	if (ptr == NULL)
 		return (NULL);
-	if(str == NULL)
+	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		ptr[i] = str[i];
+	}
+	if (ptr[i] == '\0')
+	{
+		ptr[i + 1] = '\0';
+	}
 	return (ptr);
 }
