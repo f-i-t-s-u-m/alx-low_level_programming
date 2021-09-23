a5c22c8a21a0f591b255ae98722231b4a5277abe
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-23 21:42:22 +0300

    update 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_calloc- function to assign space on memory
+ *@nmemb:int
+ *@size:int
+ *Return: address
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int a;
+	int *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(ptr) * (size * nmemb));
+	if (ptr == NULL)
+		return (NULL);
+	for (a = 0; a < (size * nmemb); a++)
+	{
+		ptr[a] = '0';
+	}
+	return (ptr);
+}
