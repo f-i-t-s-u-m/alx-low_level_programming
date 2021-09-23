1d60ecdc97819b89609aefc41d25ad71c0999331
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-23 21:08:21 +0300

    update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,22 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+/**
+ *_strlen- a function that return the length of length
+ *@str: char to check
+ *@col: int which string
+ *@n: number of string to return
+ *Return: number of string
+ */
 unsigned int _strlen(char *str, unsigned int col, unsigned int n)
 {
 	unsigned int i = 0;
 
-	if(str != NULL)
+	if (str != NULL)
 	{
-		while(str[i])
-        	i++;
-                if(col == 2 && n > i)
+		while (str[i])
+		i++;
+		if (col == 2 && n > i)
 		return (i);
-		else if(col == 2 && n < i )
+		else if (col == 2 && n < i)
 		return (n);
 	}
-	else if(str == NULL)
+	else if (str == NULL)
 	return (1);
-
 	return (i);
 }
 /**
@@ -32,21 +37,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int z = 0, i = 0;
 	char *ptr = malloc(_strlen(s1, 1, n) + (_strlen(s2, 2, n) + 1));
 
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for(; z < _strlen(s1, 1, n); z++ )
+	for (; z < _strlen(s1, 1, n); z++)
 	{
 		ptr[z] = s1[z];
 	}
-	for(; z < _strlen(s1, 1, n) + _strlen(s2, 2, n) && i < _strlen(s2, 2, n) ; z++, i++)
+	for (; z < _strlen(s1, 1, n) + _strlen(s2, 2, n) &&
+		i < _strlen(s2, 2, n) ; z++, i++)
 	{
-		if(s2 != NULL)
+		if (s2 != NULL)
 		ptr[z] = s2[i];
 		else
 		ptr[z] = ' ';
 	}
-
+	ptr[z] = '\0';
 	return (ptr);
 }
