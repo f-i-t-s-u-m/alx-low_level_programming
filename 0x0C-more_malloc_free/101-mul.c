ea7cc0ef95b66819571e8241833f4b9a4c917744
f-i-t-s-u-m <fibabirr@gmail.com>
2021-09-24 21:18:46 +0300

    update 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cnn(char **str)
+{
+ int i = 1;
+
+ 	while(str[i])
+	{
+		int z = 0;
+		while(str[i][z])
+		{
+			if(str[i][z] >= 47 && str[i][z] <= 57)
+			;
+			else
+			{
+			printf("\n%s\n", "Error");
+			exit(98);
+			}
+			z++;
+		}
+		i++;
+	}
+	return (0);
+
+}
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	if(argc != 3)
+	{
+		printf("\n%s\n", "Error");
+		exit(98);
+	}
+	else if(cnn(argv) == 0)
+	{
+		printf("\n%d\n", atoi(argv[1]) + atoi(argv[2]));
+		sum = atoi(argv[1]) + atoi(argv[2]);
+	
+	}
+
+	return (sum);
+}
