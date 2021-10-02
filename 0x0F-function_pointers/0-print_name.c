1079f4c46e50dc0abdff0518ddd20ee600e470b0
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-02 22:15:22 +0300

    update 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ *print_name - function to call f function
+ *@name: name arg
+ *@f: function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
