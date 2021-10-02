ce79c2f7b2cf6f6a658ed3110dec23cce98bd0d1
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-02 13:10:38 +0300

    update 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int a = 0;
+	
+	va_start(list, format);
+	while(*(format+a))
+	{
+		int sep = 0;
+
+		switch(*(format+a)) 
+		{
+		case 'c':
+			printf("%c", va_arg(list, int));
+			sep = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(list, int));
+			sep = 1;
+			break;
+		case 'f':
+			printf("%e", va_arg(list, double));
+			sep = 1;
+			break;
+		case 's':
+			printf("%s", va_arg(list, char *));
+			sep = 1;
+			break;
+		}
+		 if(sep == 1 && *(format + (a + 1)))
+		 {
+			 printf("%s", ", ");
+		 }
+		a++;
+
+
+	}
+	va_end(list);
+	printf("\n");
+}
