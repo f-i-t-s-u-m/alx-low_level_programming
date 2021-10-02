a639c3dde71fd9ba905c68c8dcc5742e21d18cc8
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-02 23:12:45 +0300

    update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,24 @@
 #include "function_pointers.h"
 /**
- *array_iterator - function to call f function
+ *int_index - function to call f function
  *@array: array of int
  *@size: size of aaray
- *@action: function pointer
- *
+ *@cmp: function to a pointer to compare values
+ *Return: index of match
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if(size <= 0)
+	if (size <= 0)
 		return (-1);
 	if (array == NULL || cmp == NULL)
 		return (-1);
 	while (i < size)
 	{
-		if(cmp(array[i]))
+		if (cmp(array[i]))
 		{
-		  return (i);	
+			return (i);
 		}
 	i++;
 	}
