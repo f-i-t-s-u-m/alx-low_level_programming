a978e7d99c127ce3762785c076dd0856910e9fe8
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-12 21:50:19 +0300

    update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = temp;
+	new->next = *temp;
 	*head = new;
-	return (*head);
+	return (new);
 }
