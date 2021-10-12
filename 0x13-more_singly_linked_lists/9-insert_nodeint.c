059a41a32aee3bfcb5993807e4e9fa1ce18f02d8
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-12 18:59:41 +0300

    update 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index - insert new node into linked lits
+ *@idx: index to be inserted
+ *@n: integer to new node
+ *@head: head to linked list
+ *Return: new create strutre node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current,  *temp, *new;
+	size_t i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	temp = *head;
+	for (; i < idx && temp; i++)
+	{
+		current = temp;
+		temp = temp->next;
+	}
+	current->next = new;
+	new->next = temp;
+	return (new);
+}
