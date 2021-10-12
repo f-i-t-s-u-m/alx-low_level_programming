035446409dc80737d116983c4f148df59b276d83
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-12 19:02:45 +0300

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	for (; i < idx && temp; i++)
 	{
-		if(temp->next == NULL)
+		if(temp == NULL)
 			return (NULL);
 		current = temp;
 		temp = temp->next;
