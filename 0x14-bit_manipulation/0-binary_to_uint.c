2812aae8d03b5401dc05276f73a25a86b0ff275c
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-16 13:11:31 +0300

    update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,49 +1,30 @@
 #include "main.h"
 /**
- * _strrev - function to rev string
- * @str: string to rev
- * Return: pointer to str
- */
-char *_strrev(const char *str)
-{
-	int len = 0, start = 0, end;
-	char *ptr = malloc(sizeof(*str));
-
-	if (ptr == NULL)
-		return (NULL);
-	while (str[len])
-		len++;
-	end = len - 1;
-	for (; start < len; start++)
-	{
-		ptr[start] = str[end];
-		end--;
-	}
-	return (ptr);
-}
-/**
  *binary_to_uint -a func that convert binary
  *@b: pointer to binary
  *Return: converted unsigned number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, n = 1, val = 0;
-	char *newptr = malloc(sizeof(_strrev(b)));
+	unsigned int i = 0, n = 1, val = 0, end;
 
-	newptr = _strrev(b);
-	if (newptr == NULL || !b)
+	if (!b)
 		return (0);
-	while (newptr[i])
+	while (b[i])
+		i++;
+	end = i - 1;
+	while (i > 0)
 	{
-		if (newptr[i] == 48 || newptr[i] == 49)
+		if (b[end] == '0' || b[end] == '1')
 		{
-			if (newptr[i] == 49)
+			if (b[end] == '1')
 				val += n;
 		}
 		else
 			return (0);
-		i++;
+		end--;
+		i--;
+
 		n = n * 2;
 	}
 	return (val);
