17c8b13a3eab9de2da20b61bf48017c2e27a1be8
f-i-t-s-u-m <fibabirr@gmail.com>
2021-10-19 23:03:39 +0300

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	if (fd == -1 || text_content == NULL)
 		return (-1);
 	while (text_content[len])
 		len++;
