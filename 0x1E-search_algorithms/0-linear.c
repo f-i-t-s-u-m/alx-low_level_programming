15374538e7163f5bd7aaecd6d7feb4e890fde4ea
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-30 14:49:01 +0300

    fix bettty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 /**
- * main - run main 
- * @array: array of lists
- * @size_t: size of the array,
+ * linear_search - search for value inside array
+ * @array:  array of lists
+ * @size: size of the array,
  * @value: int value
  * Return: integer
  */
 
-int linear_search(int *array, size_t size, int value) {
-	
+int linear_search(int *array, size_t size, int value)
+{
 	int i = 0;
 
-	if (!value || !array) {
+	if (!value || !array)
+	{
 		return (-1);
 	}
 
-	while(i < (int)size) {
+	while (i < (int)size)
+	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value) {
-			return i;
+		if (array[i] == value)
+		{
+			return (i);
 		}
 		i++;
 	}
